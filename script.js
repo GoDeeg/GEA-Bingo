c3a0d9d710306465bbb104d2d193e3936268615d
GoDeeg <hello.deeg@gmail.com>
2025-01-14 09:45:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const words = ["Gut isch", "Allora", "(Eigene Name)", "Mei...", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"];
+    const words = ["Gut isch", "Allora", "(Eigene Name)", "Mei...", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1", "12", "13", "14", "15", "16", "17", "18", "19", "20", "Einfach Machen"];
     const bingoCard = document.getElementById("bingo-card");
     const regenerateButton = document.getElementById("regenerate-card");
 
